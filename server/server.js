10deb262a1dad09afe93c81cfcfed0f03190d4c5
OladeleSeyi <toothy61@gmail.com>
2018-09-18 21:53:46 +0100

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+      return res.status(404).send("Invalid id");
+  } else {
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+      return res.status(404).send("no such id found");
+      }
+      res.send({todo})
+    }).catch((e)=>{
+      res.status(400).send();
+    });
+  }
+
 });
 
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} =  require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First Test to do'
 }, {
+  _id: new ObjectID(),
   text: 'Second test to do'
 }]
 
@@ -67,5 +70,33 @@ describe('GET/todos', () => {
       })
       .end(done);
   });
-  
+
+});
+
+describe('GET/todos/id', () => {
+  it('Should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+  it('should return a 404 if it does not exist', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return a 404 for invalid stuff', (done) => {
+    request(app)
+      .get(`/todos/123abc`)
+      .expect(404)
+      .expect((res) => {
+        expect(res.text).toBe('Invalid id');
+      })
+      .end(done);
+  });
 });
