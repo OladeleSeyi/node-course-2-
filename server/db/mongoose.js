8ddcd93b857631973907b9e3815389ca5d35b906
OladeleSeyi <toothy61@gmail.com>
2018-09-18 22:26:48 +0100

    Heroku Compatibility

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise  = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://toothy61:runner61@ds259912.mlab.com:59912/herokuapp');
 
 module.exports = {
   mongoose
