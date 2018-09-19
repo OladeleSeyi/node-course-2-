f113ffdc7332ce9f4fbba3a7336f0cd6d6f6838c
OladeleSeyi <toothy61@gmail.com>
2018-09-19 12:12:23 +0100

    Adding Update features

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise  = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://toothy61:runner61@ds259912.mlab.com:59912/herokuapp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp' || 'mongodb://toothy61:runner61@ds259912.mlab.com:59912/herokuapp');
 
 module.exports = {
   mongoose
