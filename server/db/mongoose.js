8562ea51d45e95d3eb19535f2e3e9102f06b2708
OladeleSeyi <toothy61@gmail.com>
2018-09-24 20:35:50 +0100

    Add GET /users and authentication

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
-require('../config/config');
 var mongoose = require('mongoose');
 
 mongoose.Promise  = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' || 'mongodb://toothy61:runner61@ds259912.mlab.com:59912/herokuapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
 }
 // Remember to return this bih
+//  || 'mongodb://toothy61:runner61@ds259912.mlab.com:59912/herokuapp'
